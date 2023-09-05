096817bc5bead4300f4a3da563d8ac384815642e
cansavvy <cansav09@gmail.com>
2023-09-05 15:17:58 -0400

    Remove readme and comments

--- 00-download-data.py
+++ 00-download-data.py
@@ -1,20 +1,15 @@
 # C. Savonen 2021
 
-# See refinebio-py tutorials for more info:
 # https://alexslemonade.github.io/refinebio-py/quickstart.html
 
 import os
 from pathlib import Path
 import pyrefinebio
 
-# Define the file path to the data directory
 data_dir = Path('data/SRP070849')
 
-# Declare the file path to the gene expression matrix file
 data_file = Path(os.path.join(data_dir, 'SRP070849.tsv'))
 
-# Declare the file path to the metadata file
-# inside the directory saved as `data_dir`
 metadata_file = Path(os.path.join(data_dir, 'metadata_SRP070849.tsv'))
 
 try:
@@ -22,9 +17,8 @@ try:
 except OSError as error:
   print(error)
 
-# Only download the data if it doesn't exist locally
 if not data_file.exists() or not metadata_file.exists():
-    
+
     print("Downloading SRP070849 from refine.bio")
 
     pyrefinebio.create_token(agree_to_terms=True, save_token=False)
